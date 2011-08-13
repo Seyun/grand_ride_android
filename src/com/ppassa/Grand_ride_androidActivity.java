e7a4753abcd1be4624eb194a8ad4423e6e7bcbbc
Wonho Kim <saunakim@gmail.com>
2011-08-13 15:47:43 -0400

    Changes

--- src/com/ppassa/Grand_ride_androidActivity.java
+++ src/com/ppassa/Grand_ride_androidActivity.java
@@ -7,6 +7,9 @@ import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.util.Log;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Grand_ride_androidActivity extends Activity {
@@ -31,6 +34,7 @@ public class Grand_ride_androidActivity extends Activity {
         
         mWebView.addJavascriptInterface(new JavaScriptInterface(mWebView), "Android");
         mWebView.loadUrl(Config.url);
+        //mWebView.loadUrl("http://192.168.157.128:3000/rides/1/show_status");
     }
     
     @Override
@@ -42,11 +46,84 @@ public class Grand_ride_androidActivity extends Activity {
         return super.onKeyDown(keyCode, event);
 
     }
+
+    class Position {
+    	double latitude;
+    	double longitude; 
+    	
+    	
+    	Position(double lat, double lon) {
+    		this.latitude = lat;
+    		this.longitude = lon;
+    	}
+    	
+    	Position() {
+    		this(0, 0);
+    	}
+    }
+
+    class PositionManager {
+    	// for temporary purpose
+    	int mCnt = 0;
+    	double[] mLats = new double[] {40.307807, 40.312846676251944, 40.323088495646196, 40.331267746544405, 40.34235722744848, 40.350305202868135};
+    	double[] mLons = new double[] {-74.6576801, -74.65995461324462, -74.64785248616943, -74.63798195699462, -74.65094239095458, -74.65699345449218};
+
+    			
+    	PositionManager() {
+    	}
+    	
+    	Position getCurrentPosition() {
+    		if (mCnt < mLats.length) {
+    			double lat = mLats[mCnt];
+    			double lon = mLons[mCnt];
+    			mCnt += 1;
+    			return new Position(lat, lon);
+    		} else {
+    			return null;
+    		}
+    	}
+    }
+    
+    class UpdatePositionTask extends TimerTask {
+    	PositionManager mPosManager;
+    	
+    	UpdatePositionTask() {
+    		mPosManager = new PositionManager();
+    	}
+    	
+    	public void run() {
+    		Log.d("timer_task", "timertask run!");
+    		Position pos = mPosManager.getCurrentPosition();
+    		if (pos != null)
+    			mWebView.loadUrl("javascript:recordTrack("+pos.latitude+", "+pos.longitude+")");
+    		else
+    			Log.d("update_task", "position is not available");
+    		return;
+    	}
+    }
     
     private class MyWebViewClient extends WebViewClient {
+    	private Timer mTimer;
+    	
+    	public MyWebViewClient() {
+    		this.mTimer = new Timer("timer_update_pos", true);
+    	}
+    	
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return false;
+        	Log.i("webviewclient", "url: "+url);
+        	if (url.startsWith("toapp://ensureTracking")) {
+        		Log.d("webviewclient", "start tracking");
+        		this.mTimer = new Timer("timer_update_pos", true);
+        		UpdatePositionTask task = new UpdatePositionTask();
+        		mTimer.schedule(task, 3000, 3000);
+        		return true;
+        	} else {
+        		Log.d("webviewclient", "leaving the page, so stop tracking");
+        		mTimer.cancel();
+        		return false;
+        	}
         }
     }
 }
+
--- src/com/ppassa/JavaScriptInterface.java
+++ src/com/ppassa/JavaScriptInterface.java
@@ -2,7 +2,6 @@ package com.ppassa;
 
 import android.webkit.WebView;
 
-
 public class JavaScriptInterface {
 	WebView webView;
 
@@ -12,5 +11,27 @@ public class JavaScriptInterface {
     
     public void ensureTracking(int ride_id) {
     	webView.loadUrl("javascript:recordTrack(1, 1)");
+    }
+    
+    public class Position {
+    	public double latitude = 0.0;
+    	public double longitude = 0.0;
+    	
+    	public Position(double lat, double lon) {
+    		this.latitude = lat;
+    		this.longitude = lon;
+    	}
+    }
+    
+    public String get_test() {
+    	return "Test";
+    }
+    
+    public double getCurrentLatitude() {
+    	return 100.;
+    }
+    
+    public double getCurrentLongitude() {
+    	return 120.;
     }
 }
