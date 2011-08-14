aef6b5a1a754fa6f9256cf210a7d9069fe46b356
Wonho Kim <saunakim@gmail.com>
2011-08-14 02:09:41 -0400

    Change

--- src/com/ppassa/Config.java
+++ src/com/ppassa/Config.java
@@ -3,6 +3,6 @@ package com.ppassa;
 public class Config {
 	//public String url = "http://grandride.heroku.com";
 	//final static public String url = "http://10.0.2.2:3000";
-	//final static public String url = "http://grandride.heroku.com";
-	final static public String url = "http://192.168.157.128:3000/rides/new";
+	final static public String url = "http://grandride.heroku.com";
+	//final static public String url = "http://192.168.157.128:3000/rides/new";
 }
